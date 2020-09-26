45b581d4a9069e591acdbf1b74a44f0456af0f6f
w.glanzer <w.glanzer@conceptive.io>
2020-09-26 10:15:53 +0200

    Added a simple menu bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {NetworkComponent} from './netplan/components/network/NetworkComponent';
+import {MenubarComponent} from './netplan/components/topbar/MenubarComponent';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <MenubarComponent/>
       <NetworkComponent/>
     </div>
   );
