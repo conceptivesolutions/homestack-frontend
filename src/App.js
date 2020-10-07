f51590922c4aca21074905a57db655d75f9c57be
w.glanzer <w.glanzer@conceptive.io>
2020-10-07 21:11:50 +0200

    Removed mdbootstrap and replaced with simpler implementations

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import {NavigationComponent} from "./netplan/components/navbar/NavigationComponent";
+import NavigationComponent from "./netplan/components/navbar/NavigationComponent";
 import {NetworkComponent} from "./netplan/components/network/NetworkComponent";
 
 export default function App()
 {
-  const style = {
-    margin: "0",
-    paddingLeft: "240px",
-    paddingTop: "0",
-  };
-
   return (
-    <Router>
-      <div className="fixed-sn black-skin h-100">
-        <NavigationComponent/>
-        <main className={"w-100 h-100"} style={style}>
-          <NetworkComponent/>
-        </main>
-      </div>
-    </Router>
+    <React.Fragment>
+      <NavigationComponent/>
+      <NetworkComponent/>
+    </React.Fragment>
   );
 };
