f0926247ae3668164264f7635bf32909ebcefb4c
w.glanzer <w.glanzer@conceptive.io>
2020-10-24 20:08:26 +0200

    feat: app should use new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./App.scss"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import DialogContainer, {dialogStore} from "./components/dialogs/DialogContainer";
 import {GlobalHooksProvider} from "@devhammed/use-global-hook";
-import {withAuthenticationRequired} from "@auth0/auth0-react";
-import {Route} from "react-router";
+import {useAuth0, withAuthenticationRequired} from "@auth0/auth0-react";
+import {Route, useHistory, useLocation} from "react-router";
 import DashboardPage from "./pages/dashboard/DashboardPage";
 import HostPage from "./pages/host/HostPage";
 import SettingsPage from "./pages/settings/SettingsPage";
 import HelpPage from "./pages/help/HelpPage";
 import LoadingIndicator from "./components/loader/LoadingIndicator";
-import TopBarComponent from "./components/topbar/TopBarComponent";
+import ContextSwitcher from "./components/contextswitcher/ContextSwitcher";
+import ContextSwitcherEntry from "./components/contextswitcher/ContextSwitcherEntry";
+import {getHosts} from "./rest/HostClient";
+import {IHost} from "./types/model";
 
 const App = () =>
+{
+  const history = useHistory();
+  const location = useLocation();
+  const {getAccessTokenSilently} = useAuth0();
+  const [hosts, setHosts] = useState<IHost[]>();
+
+  useEffect(() =>
+  {
+    getAccessTokenSilently()
+      .then((pToken) => getHosts(pToken))
+      .then((pHosts => setHosts(pHosts)))
+  }, [getAccessTokenSilently])
+
   // @ts-ignore
-  <GlobalHooksProvider hooks={[dialogStore]}>
-    <TopBarComponent className={"root__topbar"}/>
+  return <GlobalHooksProvider hooks={[dialogStore]}>
+    <ContextSwitcher className={"root_switcher"}>
+      <ContextSwitcherEntry active={location.pathname === "/"} alignment={"top"} iconName={"home"} title={"Home"} onClick={() => history.push("/")}/>
+      {hosts?.map(pHost => <ContextSwitcherEntry key={pHost.id} alignment={"top"} iconName={"server"} title={pHost.displayName || ""}
+                                                 active={location.pathname.startsWith("/hosts/" + pHost.id)}
+                                                 onClick={() => history.push("/hosts/" + pHost.id)}/>)}
+      <ContextSwitcherEntry alignment={"bottom"} iconName={"plus"} title={"Add System"}/>
+    </ContextSwitcher>
     <div className={"root_content"}>
       <Route exact path={"/"} component={DashboardPage}/>
       <Route exact path={"/hosts/:hostID"} component={HostPage}/>
@@ -23,7 +45,8 @@ const App = () =>
       <Route exact path={"/help"} component={HelpPage}/>
     </div>
     <DialogContainer/>
-  </GlobalHooksProvider>
+  </GlobalHooksProvider>;
+}
 
 export default withAuthenticationRequired(App, {
   onRedirecting: () => <LoadingIndicator/>,
