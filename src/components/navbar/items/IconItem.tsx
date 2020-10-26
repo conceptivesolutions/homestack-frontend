f033f58ce95db169eeaf54a29d39068d382211c6
w.glanzer <w.glanzer@conceptive.io>
2020-10-26 18:35:44 +0100

    feat: added "active" property to iconitem

--- src/components/navbar/items/IconItem.tsx
+++ src/components/navbar/items/IconItem.tsx
@@ -5,12 +5,13 @@ import NavBarItem from "../NavBarItem";
 interface IIconItem
 {
   alignment: 'left' | 'right',
+  active?: boolean,
   iconName: string,
   onClick?: () => void,
 }
 
 export default (props: IIconItem) => (
-  <NavBarItem onClick={props.onClick} alignment={props.alignment}>
+  <NavBarItem onClick={props.onClick} active={props.active} alignment={props.alignment}>
     <span className={"iconitem fa fa-" + props.iconName}/>
   </NavBarItem>
 )
