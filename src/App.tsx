81808f3bc8a84eca3cf38f6dd20806765ad620c5
w.glanzer <w.glanzer@conceptive.io>
2020-10-28 20:37:08 +0100

    refactor: moved context switcher to page container

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,25 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import "./App.scss"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import DialogContainer, {dialogStore} from "./components/dialogs/DialogContainer";
 import {GlobalHooksProvider} from "@devhammed/use-global-hook";
 import {withAuthenticationRequired} from "@auth0/auth0-react";
-import {Route, useHistory, useLocation} from "react-router";
+import {Route, Switch} from "react-router";
 import HostPage from "./pages/host/HostPage";
 import SettingsPage from "./pages/settings/SettingsPage";
 import HelpPage from "./pages/help/HelpPage";
 import LoadingIndicator from "./components/loader/LoadingIndicator";
-import ContextSwitcher from "./components/contextswitcher/ContextSwitcher";
-import ContextSwitcherEntry from "./components/contextswitcher/ContextSwitcherEntry";
-import {GlobalContext} from "./state/GlobalContext";
-import _ from "lodash";
-import randomColor from 'randomcolor';
 
 const App = () =>
 {
-  const history = useHistory();
-  const location = useLocation();
-  const {state} = useContext(GlobalContext)
-
   return (
     // @ts-ignore
     <GlobalHooksProvider hooks={[dialogStore]}>
-      <ContextSwitcher className={"root_switcher"}>
-        <ContextSwitcherEntry active={location.pathname === "/"} alignment={"top"} iconName={"home"} title={"Home"}/>
-        {_.sortBy(state.hosts, ['displayName', 'id']).map(pHost => <ContextSwitcherEntry key={pHost.id} alignment={"top"} iconName={"server"}
-                                                                                         title={pHost.displayName || ""}
-                                                                                         active={location.pathname.startsWith("/hosts/" + pHost.id)}
-                                                                                         iconColor={"white"}
-                                                                                         color={randomColor({
-                                                                                           hue: "blue",
-                                                                                           seed: pHost.id,
-                                                                                           luminosity: "bright"
-                                                                                         })}
-                                                                                         onClick={() => history.push("/hosts/" + pHost.id)}/>)}
-        <ContextSwitcherEntry alignment={"bottom"} iconName={"plus"} title={"Add System"}/>
-      </ContextSwitcher>
-      <div className={"root_content"}>
+      <Switch>
         <Route exact path={"/hosts/:hostID"} component={HostPage}/>
         <Route exact path={"/settings"} component={SettingsPage}/>
         <Route exact path={"/help"} component={HelpPage}/>
-      </div>
+      </Switch>
       <DialogContainer/>
     </GlobalHooksProvider>
   );
--- src/components/page/PageContainer.tsx
+++ src/components/page/PageContainer.tsx
@@ -4,9 +4,14 @@ import NavBar from "../navbar/NavBar";
 import NavBarItem, {INavBarItem} from "../navbar/NavBarItem";
 import IconItem from "../navbar/items/IconItem";
 import ProfileItem from "../navbar/items/ProfileItem";
-import {useHistory} from "react-router";
+import {useHistory, useLocation} from "react-router";
 import {GlobalContext} from "../../state/GlobalContext";
 import classNames from "classnames";
+import ContextSwitcherEntry from "../contextswitcher/ContextSwitcherEntry";
+import _ from "lodash";
+import randomColor from "randomcolor";
+import ContextSwitcher from "../contextswitcher/ContextSwitcher";
+import {IHost} from "../../types/model";
 
 export interface IPageContent
 {
@@ -19,10 +24,16 @@ export interface IPageContent
 export default (props: IPageContent) =>
 {
   const history = useHistory();
-  const {state: {user}} = useContext(GlobalContext);
+  const location = useLocation();
+  const {state: {user, hosts}} = useContext(GlobalContext);
 
   return (
     <div className={classNames("pagecontent__container", {"pagecontent__container_with-navigator": !!props.navigator})}>
+      <ContextSwitcher className={"pagecontent__switcher"}>
+        <ContextSwitcherEntry active={location.pathname === "/"} alignment={"top"} iconName={"home"} title={"Home"}/>
+        {_createHostSwitcherEntries(hosts, location.pathname, hostID => history.push("/hosts/" + hostID))}
+        <ContextSwitcherEntry alignment={"bottom"} iconName={"plus"} title={"Add System"}/>
+      </ContextSwitcher>
       <NavBar className={"pagecontent__navbar"}>
         {props.navbarItems?.map(pItem => <NavBarItem key={JSON.stringify(pItem.children)} {...pItem}/>)}
         <IconItem alignment={"right"} iconName={"cog"} active={history.location.pathname.startsWith("/settings")} onClick={() => history.push("/settings")}/>
@@ -40,4 +51,26 @@ export default (props: IPageContent) =>
       </div>
     </div>
   );
+}
+
+/**
+ * Creates the host entries for the switcher
+ *
+ * @param hosts current hosts
+ * @param currentLocation current location
+ * @param onClick function that gets executed on click
+ */
+function _createHostSwitcherEntries(hosts: IHost[] | undefined, currentLocation: string, onClick: (hostID: string) => void)
+{
+  return _.sortBy(hosts, ['displayName', 'id'])
+    .map(pHost => <ContextSwitcherEntry key={pHost.id} alignment={"top"} iconName={"server"}
+                                        title={pHost.displayName || ""}
+                                        active={currentLocation.startsWith("/hosts/" + pHost.id)}
+                                        iconColor={"white"}
+                                        color={randomColor({
+                                          hue: "blue",
+                                          seed: pHost.id,
+                                          luminosity: "bright"
+                                        })}
+                                        onClick={() => onClick(pHost.id)}/>)
 }
