c68dc6a0c73ddc7cc9cb90576999d104005b1246
w.glanzer <w.glanzer@conceptive.io>
2020-10-28 21:50:36 +0100

    feat: added selection behavior for navigation tree

--- src/components/tree/NavigationTree.tsx
+++ src/components/tree/NavigationTree.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./NavigationTree.scss";
 import Tree, {DefaultNodeProps, Loader} from "react-hyper-tree";
 import classNames from "classnames";
+import {TreeView} from "react-hyper-tree/dist/helpers/node";
 
 export interface INavigationTree
 {
   required: any,
   handlers: any,
+  instance: TreeView,
   className?: string,
 }
 
@@ -15,8 +17,10 @@ export interface ITreeNode
   id: string,
   name: string,
   iconName: string,
+  selectable?: boolean,
   iconColor?: string,
   children?: ITreeNode[],
+  onSelect?: (selected: boolean) => void,
 }
 
 export default (props: INavigationTree) => (
@@ -41,7 +45,7 @@ export default (props: INavigationTree) => (
         strokeWidth: 1,
         strokeDasharray: '4 2',
       }}
-      renderNode={_renderNode}
+      renderNode={(renderProps) => _renderNode(props.instance, renderProps)}
     />
   </div>
 )
@@ -49,13 +53,28 @@ export default (props: INavigationTree) => (
 /**
  * Renders a single node of the tree
  *
+ * @param instance
  * @param node
  * @param onSelect
  * @param onToggle
  */
-function _renderNode({node, onSelect, onToggle}: DefaultNodeProps)
+function _renderNode(instance: TreeView, {node, onSelect, onToggle}: DefaultNodeProps)
 {
-  return <div className={classNames("component-tree__node", {"component-tree__node-root": node.options.root})} onClick={onSelect}>
+  return <div className={classNames("component-tree__node", {
+    "component-tree__node-root": node.options.root,
+    "component-tree__node-selectable": node.data.selectable,
+    "component-tree__node-selected": node.options.selected,
+  })} onClick={(e) =>
+  {
+    if (!node.data.selectable)
+      return;
+
+    instance.unselectAll();
+    onSelect(e);
+
+    if (node.data.onSelect)
+      node.data.onSelect(node.isSelected());
+  }}>
     {_createControlNode({node, onSelect, onToggle})}
     {node.isLoading() && (<Loader/>)}
     {node.data.iconName && <span style={{color: node.data.iconColor}} className={"component-tree__node-icon fa fa-" + node.data.iconName}/>}
--- src/pages/host/navigator/NavigatorComponent.tsx
+++ src/pages/host/navigator/NavigatorComponent.tsx
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect} from "react";
 import "./NavigatorComponent.scss";
-import {ACTION_CREATE_DEVICE, ACTION_REMOVE_DEVICE, HostContext} from "../state/HostContext";
+import {ACTION_CREATE_DEVICE, ACTION_REMOVE_DEVICE, EHostStateActions, HostContext, HostDispatch} from "../state/HostContext";
 import {useTreeState} from "react-hyper-tree";
 import _ from "lodash";
 import NavigationTree, {ITreeNode} from "../../../components/tree/NavigationTree";
@@ -26,6 +26,8 @@ export default ({className}: { className: string }) =>
       name: pDevice.address || "unknown",
       iconName: "network-wired",
       iconColor: getStateColor(pDevice.metrics),
+      selectable: true,
+      onSelect: selected => _onSelect(pDevice.id, selected, dispatch),
       children: (pDevice.metrics || []).map(pMetric => ({
         id: pDevice.id + pMetric.type,
         name: pMetric.type + " => " + pMetric.state,
@@ -35,25 +37,43 @@ export default ({className}: { className: string }) =>
     }))
   };
 
-  const {required, handlers} = useTreeState({
+  const {required, instance, handlers} = useTreeState({
     id: "deviceTree",
-    multipleSelect: false,
+    multipleSelect: true,
     data: root,
   });
 
+  // Always open root
   useEffect(() =>
   {
     handlers.setOpen("root")
   }, [handlers])
 
+  // Selection
+  useEffect(() =>
+  {
+    instance.unselectAll();
+    state.selection?.devices?.forEach(pDeviceID => handlers.setSelected(pDeviceID, true))
+  }, [state.selection, instance, handlers])
+
   return (
     <div className={classNames(className, "navigator__container")}>
-      <NavigationTree className={"navigator__tree"} required={required} handlers={handlers}/>
+      <NavigationTree className={"navigator__tree"} required={required} instance={instance} handlers={handlers}/>
       <ActionList className={"navigator__actions"}>
         <ActionListItem name={"Add Device"} iconName={"plus-circle"} onClick={() => dispatch(ACTION_CREATE_DEVICE())}/>
         <ActionListItem disabled={_.isEmpty(state.selection?.devices)} name={"Remove Device"} iconName={"minus-circle"}
-                        color={"red"} onClick={() => state.selection?.devices.forEach(pDevID => dispatch(ACTION_REMOVE_DEVICE(pDevID)))}/>
+                        color={"red"} onClick={() => state.selection?.devices?.forEach(pDevID => dispatch(ACTION_REMOVE_DEVICE(pDevID)))}/>
       </ActionList>
     </div>
   );
+}
+
+function _onSelect(nodeID: string, selected: boolean, dispatch: HostDispatch)
+{
+  if (selected)
+    dispatch({
+      type: EHostStateActions.SET_SELECTION, payload: {
+        devices: [nodeID]
+      }
+    });
 }
