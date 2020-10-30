5a87a628ebc409f4450c2ccdc657e6cad038fb3f
w.glanzer <w.glanzer@conceptive.io>
2020-10-30 20:54:40 +0100

    refactor: renamed "_app.js" to "_app.tsx", including scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './index.scss';
+import './_app.scss';
 import {GlobalHooksProvider} from "@devhammed/use-global-hook";
 import DialogContainer, {dialogStore} from "../components/dialogs/DialogContainer";
 import {GlobalProvider} from "../state/GlobalContext";
 
 // noinspection JSUnusedGlobalSymbols
-export default function App({Component, pageProps})
+export default function App({Component, pageProps}: { Component: any, pageProps: any })
 {
   return (
     <GlobalProvider>
+      {/** @ts-ignore */}
       <GlobalHooksProvider hooks={[dialogStore]}>
         <Component {...pageProps} />
         <DialogContainer/>
