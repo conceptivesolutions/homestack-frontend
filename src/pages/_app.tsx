842927fe8fd442a3d66eeb736f26b7ecbaad39ea
w.glanzer <w.glanzer@conceptive.io>
2020-11-08 20:16:52 +0100

    feat: added loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import './_app.scss';
+import 'nprogress/nprogress.css';
 import {GlobalHooksProvider} from "@devhammed/use-global-hook";
 import DialogContainer, {dialogStore} from "../components/dialogs/DialogContainer";
 import {GlobalProvider} from "../context/GlobalContext";
 import {AuthProvider} from "../context/AuthContext";
+import {Router} from "next/router";
+import NProgress from 'nprogress';
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 // noinspection JSUnusedGlobalSymbols
 export default function App({Component, pageProps}: { Component: any, pageProps: any })
