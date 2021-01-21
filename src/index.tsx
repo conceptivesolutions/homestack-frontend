a4636c7e868034a615122d93ecfafa8fd0eb9d2c
w.glanzer <w.glanzer@conceptive.io>
2021-01-21 21:55:21 +0100

    chore: removed eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
+import { App } from '_app';
 import 'index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './_app';
 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
