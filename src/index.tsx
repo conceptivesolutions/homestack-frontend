80bb2113b12fbe5bb96d1c5327bb0006810e0d91
w.glanzer <w.glanzer@conceptive.io>
2021-01-21 20:29:45 +0100

    feat: added initial index page

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import 'index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './_app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
