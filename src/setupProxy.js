6886deb0306a19ecbfb7e9978620cadf8fa9aa71
w.glanzer <w.glanzer@conceptive.io>
2021-01-21 20:21:47 +0100

    feat: added scss support and defined api proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+module.exports = function (app)
+{
+  app.use('/api', createProxyMiddleware({
+    target: process.env.API_TARGET,
+    changeOrigin: true,
+  }));
+};
