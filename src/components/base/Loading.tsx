0a76cc8e307a6fa8e2787887bbdad956a14358b1
w.glanzer <w.glanzer@conceptive.io>
2021-01-24 16:43:39 +0100

    feat: calculate loading spinners size

--- src/components/base/Loading.tsx
+++ src/components/base/Loading.tsx
@@ -1,11 +1,20 @@
+import classNames from "classnames";
 import React from 'react';
 import styles from "./Loading.module.scss"
 
-export const Loading: React.VFC = () => (
+type LoadingProps = {
+  size: number,
+}
+
+export const Loading: React.VFC<LoadingProps> = ({size}) => (
   <div className={styles.container}>
-    <div className={styles.loadingioSpinner}>
+    <div className={styles.loadingioSpinner} style={{width: size + "rem", height: size + "rem"}}>
       <div className={styles.loading}>
-        <div/>
+        <div className={classNames({
+          [styles.loadingBig]: size >= 5,
+          [styles.loadingMid]: size < 5 && size > 2,
+          [styles.loadingSmall]: size <= 2,
+        })}/>
       </div>
     </div>
   </div>
