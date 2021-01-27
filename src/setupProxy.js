99d0b1cbbe88f0523ad07c581f8fd24a2383451d
w.glanzer <w.glanzer@conceptive.io>
2021-01-27 17:09:05 +0100

    fix: failed to forward POST / PUT services to proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
 
 module.exports = function (app)
 {
+  // parse JSON
+  app.use(bodyParser.json());
+
   // proxy to authentication server
   app.use('/api/auth', createProxyMiddleware({
     target: process.env.AUTH_TARGET,
@@ -28,6 +32,9 @@ module.exports = function (app)
   // proxy to backend
   app.use('/api', createProxyMiddleware({
     target: process.env.API_TARGET,
+    pathRewrite: {
+      '^/api': ''
+    },
     changeOrigin: true,
   }));
 };
