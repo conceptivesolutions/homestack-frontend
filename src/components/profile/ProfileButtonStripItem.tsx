b92e4442468d215cc59a8ec1de3a6df018ba40c8
w.glanzer <w.glanzer@conceptive.io>
2021-02-27 22:03:45 +0100

    fix: click outside in profile item not closing popup

--- src/components/profile/ProfileButtonStripItem.tsx
+++ src/components/profile/ProfileButtonStripItem.tsx
@@ -2,38 +2,31 @@ import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
 import Icon from "@mdi/react";
 import { ButtonStripItem } from "components/base/list/ButtonStrip";
 import { Popup } from "components/base/Popup";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
 import styles from "./ProfileButtonStripItem.module.scss";
 
 type ProfileButtonStripItemProps = {
   iconSrc?: string
 };
 
-export const ProfileButtonStripItem: React.FC<ProfileButtonStripItemProps> = ({iconSrc, children}) =>
+export const ProfileButtonStripItem: React.FC<ProfileButtonStripItemProps> = ({ iconSrc, children }) =>
 {
   const [popupOpen, setPopupOpen] = useState<boolean>(false)
 
-  useEffect(() =>
-  {
-    if (!popupOpen)
-      return;
-
-    const listener = () => setPopupOpen(false)
-    window.addEventListener("click", listener)
-    return () => window.removeEventListener("click", listener)
-  }, [popupOpen, setPopupOpen])
-
   return (
-    <ButtonStripItem key={"profileitem"} className={styles.container} onClick={() => setPopupOpen(pV => !pV)}>
-      <div className={styles.innerContainer}>
-        {iconSrc ?
-          <img className={styles.icon} src={iconSrc} alt={"avatar"}/> :
-          <div className={styles.icon}/>}
-        {<Icon path={popupOpen ? mdiChevronUp : mdiChevronDown} className={styles.dropdownArrow} size={1}/>}
-      </div>
-      <Popup open={popupOpen} alignment={"right"}>
-        {children}
-      </Popup>
-    </ButtonStripItem>
+    <OutsideClickHandler display={"flex"} onOutsideClick={() => setPopupOpen(false)}>
+      <ButtonStripItem key={"profileitem"} className={styles.container} onClick={() => setPopupOpen(pV => !pV)}>
+        <div className={styles.innerContainer}>
+          {iconSrc ?
+            <img className={styles.icon} src={iconSrc} alt={"avatar"}/> :
+            <div className={styles.icon}/>}
+          {<Icon path={popupOpen ? mdiChevronUp : mdiChevronDown} className={styles.dropdownArrow} size={1}/>}
+        </div>
+        <Popup className={styles.popup} open={popupOpen} alignment={"right"}>
+          {children}
+        </Popup>
+      </ButtonStripItem>
+    </OutsideClickHandler>
   );
 };
